61558e569aa3656bc589de63a24eada59e44f6aa
Ted Tran <ted.tran6@gmail.com>
2018-10-20 11:47:01 -0500

    Add files via upload

---
+++ another_fan_test.ino
@@ -0,0 +1,66 @@
+int NbTopsFan;      //Varibles used for RPM calculations
+int CalculateRMP;           //the rmp calculated
+int hallsensor = 2; //The pin location of the RPM sensor??? no it's not
+               
+typedef struct{                  //Defines the structure for multiple fans and their dividers
+  char fantype;
+  unsigned int fandiv;
+}fanspec;
+
+fanspec fanspace[3]={{0,1},{1,2},{2,8}}; //Definitions of the fans
+
+char fan = 1;   //This is the varible used to select the fan and it's divider, set 1 for unipole hall effect sensor 
+               //and 2 for bipole hall effect sensor 
+
+void rpm ()      //This is the function that the interupt calls 
+{ 
+ NbTopsFan++; 
+}
+
+const long intervals = 124;
+long currentTime;
+long previousTime = 0;
+
+boolean enabled = false;
+
+int fanVal;
+int lastFanVal = 0;
+
+void setup() {
+  Serial.begin(115200);
+  attachInterrupt(0, rpm, RISING);
+}
+
+void loop() {
+    //FAN STUFF-------------------------------------------------------------
+    currentTime = millis();
+
+    if(enabled == false){
+     NbTopsFan = 0;  //Set NbTops to 0 ready for calculations
+     sei();   //Enables interrupts
+     enabled = true;
+    }
+
+   if(currentTime - previousTime >= intervals){
+     previousTime = currentTime;
+     cli();   //Disable interrupts
+     enabled = false;
+     CalculateRMP = ((NbTopsFan * 480)/fanspace[fan].fandiv);
+     if(CalculateRMP > 3600)
+      CalculateRMP = 3600;
+      
+     fanVal = map(CalculateRMP, 0, 3600, 0, 123);
+     if(fanVal > lastFanVal + 3 || fanVal < lastFanVal - 3){
+      MIDIMessage(174, 1, fanVal);
+      lastFanVal = fanVal;
+     }
+   }
+}
+
+
+void MIDIMessage(byte command, byte data1, byte data2) //pass values out through standard Midi Command
+{
+   Serial.write(command);
+   Serial.write(data1);
+   Serial.write(data2);
+}
---
+++ powerswitch_tail_test.ino
@@ -0,0 +1,43 @@
+// Testing the use of the powerswitch tail 
+// control powerSocket
+const int powerPin = A0;
+const int ledPin = 13;
+boolean powerOn = false;
+
+// control LEDs
+
+int incomingByte;  //a variable to read incoming serial data 
+
+void setup() {
+  //initiate serial communication:
+  Serial.begin(9600);
+  Serial.println(F("starting up"));
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(powerPin, OUTPUT);
+
+  digitalWrite(ledPin, LOW);
+  digitalWrite(powerPin, HIGH);
+}  
+
+void loop() {
+
+  // read serial messages
+  if (Serial.available() > 0) {
+
+    powerOn = !powerOn;
+    
+    if (powerOn) {
+      digitalWrite(ledPin, HIGH);
+      digitalWrite(powerPin, LOW);
+      Serial.println(F("ON"));
+    } else {
+      digitalWrite(ledPin, LOW);
+      digitalWrite(powerPin, HIGH);
+      Serial.println(F("OFF"));
+    }      
+    
+    //read all the bytes in the serial buffer (we don't care about the contents of the data)
+    while (Serial.available()) incomingByte = Serial.read();
+  }                       
+}
